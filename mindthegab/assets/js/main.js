dfc02b3c964a5ff609b5bcb1c6db4a78df0beb49
paulkazusek <paul.kazusek@gmail.com>
2024-12-09 17:51:25 +0100

    add custom javascript

--- mindthegab/assets/js/main.js
+++ mindthegab/assets/js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-    
+/**
+ * Custom JavaScript for MindTheGab Theme
+ * Description: Custom JS functions for the theme
+ * Author: Paul Kazusek
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('JavaScript-Datei erfolgreich geladen!');
 });
