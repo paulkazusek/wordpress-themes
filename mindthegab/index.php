3041906ae42d9b237edcabc7bbc5a7f00619efdc
paulkazusek <paul.kazusek@gmail.com>
2024-12-09 22:13:39 +0100

    add index.php

---
+++ mindthegab/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <title><?php bloginfo( 'name' ); ?></title>
+    <!-- Meta -->
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="" >
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+</head>
+<body>
+    <h1>Hello WordPress<h1> 
+</body>
+</html>
