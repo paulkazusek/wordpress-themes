54a83a891a805d761637cb615f2c1eaf567770da
paulkazusek <paul.kazusek@gmail.com>
2024-12-09 17:53:07 +0100

    add custom javascript

---
+++ pkfotodesign/assets/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Custom JavaScript for MindTheGab Theme
+ * Description: Custom JS functions for the theme
+ * Author: Paul Kazusek
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('JavaScript-Datei erfolgreich geladen!');
+});
