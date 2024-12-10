f873351854199d044bc3eef67484e89992d51700
paulkazusek <paul.kazusek@gmail.com>
2024-12-10 16:45:46 +0100

    add Bootstrap, Floating UI

--- mindthegab/index.php
+++ mindthegab/index.php
@@ -8,11 +8,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="mind the gab" >
     <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+    <?php 
+    if (function_exists( 'wp_body_open' )) {
+        wp_body_open();
+    } 
+    ?>
+
     <h1>Hello WordPress</h1>
+    
 
     <?php wp_footer(); ?>
 </body>
